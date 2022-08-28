f1941fd10c1052910599dfe5a40c2bebc8c114dd
Yuyama-Syo <yuyamasyou@icloud.com>
2022-08-28 21:44:44 +0900

    pre,boot

--- resources/views/posts/boot.blade.php
+++ resources/views/posts/boot.blade.php
@@ -3,12 +3,10 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Laravel</title>
-
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <!-- Styles -->
         <style>
             html, body {
@@ -19,35 +17,28 @@
                 height: 100vh;
                 margin: 0;
             }
-
             .full-height {
                 height: 100vh;
             }
-
             .flex-center {
                 align-items: center;
                 display: flex;
                 justify-content: center;
             }
-
             .position-ref {
                 position: relative;
             }
-
             .top-right {
                 position: absolute;
                 right: 10px;
                 top: 18px;
             }
-
             .content {
                 text-align: center;
             }
-
             .title {
                 font-size: 84px;
             }
-
             .links > a {
                 color: #636b6f;
                 padding: 0 25px;
@@ -57,7 +48,6 @@
                 text-decoration: none;
                 text-transform: uppercase;
             }
-
             .m-b-md {
                 margin-bottom: 30px;
             }
@@ -71,28 +61,26 @@
                         <a href="{{ url('/home') }}">Home</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
-
+ 
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}">Register</a>
                         @endif
                     @endauth
                 </div>
             @endif
-
             <div class="content">
                 <div class="title m-b-md">
                     Laravel
                 </div>
-
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://laravel.com/docs"><button class='btn btn-default'>Docs</button></a>
+                    <a href="https://laracasts.com"><button class='btn btn-primary'>Laracasts</button></a>
+                    <a href="https://laravel-news.com"><button class='btn btn-success'>News</button></a>
+                    <a href="https://blog.laravel.com"><button class='btn btn-info'>Blog</button></a>
+                    <a href="https://nova.laravel.com"><button class='btn btn-warning'>Nova</button></a>
+                    <a href="https://forge.laravel.com"><button class='btn btn-danger'>Forge</button></a>
+                    <a href="https://vapor.laravel.com"><button class='btn btn-link'>Vapor</button></a>
+                    <a href="https://github.com/laravel/laravel"><button class='btn btn-primary'>GitHub</button></a>
                 </div>
             </div>
         </div>
