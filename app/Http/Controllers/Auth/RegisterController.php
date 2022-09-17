3377080ecee67a99c5666f9cd4c17a7da8e1f4ed
Yuyama-Syo <yuyamasyou@icloud.com>
2022-09-17 12:49:47 +0900

    web.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\User;
+use App\Profile;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -91,8 +92,11 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+        ]);
+        
+        return Profile::create([
             'self_introduction'=>$data['self_introduction'],
-            'image_path'=>$fileName,
+            'image_path'=>$fillName,
         ]);
     }
     
